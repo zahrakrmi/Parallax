3514d4f9bbbbb4b824b31de7326ab1514e972c67
zahra karami <69428157+zahrakrmi@users.noreply.github.com>
2025-01-03 08:43:25 +0330

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React, { useState } from "react";
+
+
+const App = () => {
+ 
+
+  return (
+    <section className="w-full h-full  flex items-center justify-center bg-green-300">
+      <h1 className="text-4xl font-bold border text-white">Green Section</h1>
+    </section>
+
+  );
+};
+
+export default App;
